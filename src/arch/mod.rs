9b213a15bc762c417eccd235a9f466f27a3dc058
xjunko <phony@kafu.ovh>
2026-02-28 13:23:22 +0800

    cpu: enable SSE if supported else crash out

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -4,9 +4,34 @@ pub mod interrupts;
 
 use core::arch::asm;
 
+use raw_cpuid::CpuId;
+use x86_64::registers::control::{Cr0, Cr0Flags, Cr4, Cr4Flags};
+
 use crate::info;
 
+fn install_features() {
+    let cpuid = CpuId::new();
+    if let Some(finfo) = cpuid.get_feature_info() {
+        if finfo.has_sse() {
+            unsafe {
+                Cr0::update(|flags| {
+                    flags.remove(Cr0Flags::EMULATE_COPROCESSOR | Cr0Flags::TASK_SWITCHED);
+                    flags.insert(Cr0Flags::MONITOR_COPROCESSOR);
+                });
+
+                Cr4::update(|flags: &mut Cr4Flags| {
+                    flags.insert(Cr4Flags::OSFXSR | Cr4Flags::OSXMMEXCPT_ENABLE);
+                });
+            }
+            info!("SSE enabled");
+        } else {
+            panic!("cpu does not support SSE");
+        }
+    }
+}
+
 pub fn install() {
+    install_features();
     gdt::install();
     info!("GDT installed.");
     idt::install();
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 #![feature(abi_x86_interrupt)]
 #![allow(dead_code)]
 #![allow(clippy::manual_div_ceil)]
-use alloc::vec;
 
 extern crate alloc;
 
@@ -13,11 +12,13 @@ mod boot;
 mod drivers;
 mod system;
 
+use alloc::vec;
+
 #[unsafe(no_mangle)]
 unsafe extern "C" fn kmain() -> ! {
     assert!(boot::limine::BASE_REVISION.is_supported());
-
-    drivers::logging::install();
+    // com1 serial logging
+    drivers::tty::serial::install();
 
     // cpu init
     arch::install();
@@ -27,11 +28,16 @@ unsafe extern "C" fn kmain() -> ! {
     system::mem::vmm::install();
     system::mem::heap::install().expect("failed to install heap");
 
-    // memory test
+    // past this point, the kernel can now do dynamic allocation
+    drivers::tty::flanterm::install();
+
+    // self test, more to be added.
     system::mem::self_test();
 
-    // test vfs
+    // vfs test
     system::vfs::install();
+
+    // file reading test
     let file = system::vfs::open("/init/hello.txt", 0).expect("failed to open file");
     let metadata = file.metadata().expect("failed to get metadata");
     info!("file size: {} bytes", metadata.size);
@@ -41,7 +47,7 @@ unsafe extern "C" fn kmain() -> ! {
     info!("read {} bytes from file", bytes_read);
     info!(
         "file contents: {}",
-        core::str::from_utf8(&buf).unwrap_or("<invalid utf-8>")
+        core::str::from_utf8(&buf).expect("invalid contents")
     );
 
     // test breakpoint
