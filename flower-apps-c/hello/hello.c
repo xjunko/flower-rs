860ff420b21579c0b6acdba0c708033eb25cc0f4
xjunko <phony@kafu.ovh>
2026-03-20 20:33:03 +0800

    apps-c: c programs example

---
+++ flower-apps-c/hello/hello.c
@@ -0,0 +1,13 @@
+void main()
+{
+    int fd = 1;
+    const char *msg = "hello world from userspace in c!\n";
+    int len = 34;
+
+    asm volatile(
+        "syscall"
+        :
+        : "a"(1), "D"(fd), "S"(msg), "d"(len));
+
+    asm volatile("syscall" ::"a"(0));
+}
