bed70a8cf845568a994a4b1111dfde5e95680e4c
xjunko <phony@kafu.ovh>
2026-03-20 22:15:45 +0800

    libc: add support for sleep (ms only)

--- flower-libc/src/std.rs
+++ flower-libc/src/std.rs
@@ -1,4 +1,4 @@
-use flower_mono::syscalls::{SYS_EXIT, SYS_WRITE};
+use flower_mono::syscalls::{SYS_EXIT, SYS_MSLEEP, SYS_WRITE};
 
 use crate::syscalls::{syscall1, syscall3};
 
@@ -10,3 +10,5 @@ pub fn exit(s: u64) -> ! {
 pub fn write(fd: u64, buf: &[u8]) -> usize {
     syscall3(SYS_WRITE, fd, buf.as_ptr() as u64, buf.len() as u64) as usize
 }
+
+pub fn sleep(millis: u64) { syscall1(SYS_MSLEEP, millis); }
