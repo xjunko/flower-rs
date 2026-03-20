d973c802f56585ff6ef9accf1e118881688288b0
xjunko <phony@kafu.ovh>
2026-03-20 22:15:35 +0800

    mono: add more syscall defs

--- flower-mono/src/syscalls.rs
+++ flower-mono/src/syscalls.rs
@@ -1,2 +1,8 @@
-pub const SYS_EXIT: u64 = 0;
+pub const SYS_READ: u64 = 0;
 pub const SYS_WRITE: u64 = 1;
+pub const SYS_OPEN: u64 = 2;
+pub const SYS_CLOSE: u64 = 3;
+
+pub const SYS_EXIT: u64 = 60;
+
+pub const SYS_MSLEEP: u64 = 101;
