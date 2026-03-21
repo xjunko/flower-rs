8a410edd099c86afb333aee945fe6f6c562881e8
xjunko <phony@kafu.ovh>
2026-03-22 00:33:00 +0800

    mono: update syscalls defs

--- flower-mono/src/syscalls.rs
+++ flower-mono/src/syscalls.rs
@@ -1,8 +1,17 @@
-pub const SYS_READ: u64 = 0;
-pub const SYS_WRITE: u64 = 1;
-pub const SYS_OPEN: u64 = 2;
-pub const SYS_CLOSE: u64 = 3;
+pub const SYS_RESTART: u64 = 0;
+pub const SYS_EXIT: u64 = 1;
+pub const SYS_FORK: u64 = 2;
+pub const SYS_READ: u64 = 3;
+pub const SYS_WRITE: u64 = 4;
+pub const SYS_OPEN: u64 = 5;
+pub const SYS_CLOSE: u64 = 6;
+pub const SYS_SEEK: u64 = 8;
 
-pub const SYS_EXIT: u64 = 60;
+pub const SYS_WRITE_FS_BASE: u64 = 29;
+
+pub const SYS_GET_THREAD_ID: u64 = 30;
+
+pub const SYS_MMAP: u64 = 31;
+pub const SYS_MUNMAP: u64 = 32;
 
 pub const SYS_MSLEEP: u64 = 101;
