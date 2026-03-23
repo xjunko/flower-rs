57dc80f53c099b6fd4d6a2e085810652f0429755
xjunko <phony@kafu.ovh>
2026-03-24 06:44:42 +0800

    kernel: change shell path

--- flower-kernel/src/user.rs
+++ flower-kernel/src/user.rs
@@ -18,16 +18,17 @@ Memory:   {}/{}MB
     )
 }
 
+const SHELL_PATH: &str = "/init/bin/shell";
 pub fn entry() {
     logo();
 
-    if let Ok(file) = system::vfs::open("/init/shell", 0) {
+    if let Ok(file) = system::vfs::open(SHELL_PATH, 0) {
         let metadata = file.metadata().expect("invalid metadata");
         let mut buffer = alloc::vec![0u8; metadata.size ];
         file.read(&mut buffer).expect("failed to read file");
         system::proc::spawn_elf("shell", &buffer)
             .expect("failed to spawn shell process");
     } else {
-        println!("failed to open file /init/shell");
+        log::error!("failed to open file {}", SHELL_PATH);
     }
 }
