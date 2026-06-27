59ab95ba243932feb30c1fb00409930fa2567995
xjunko <phony@kafu.ovh>
2026-06-27 22:37:45 +0800

    kernel: 1mb user stack

--- flower-kernel/src/arch/x86_64/layout.rs
+++ flower-kernel/src/arch/x86_64/layout.rs
@@ -6,5 +6,5 @@ pub const KERNEL_HEAP_START: usize = 0xFFFF_9000_0000_0000;
 pub const KERNEL_HEAP_SIZE: usize = 1024 * 1024;
 
 pub const USER_STACK_TOP_PAGE: u64 = 0x0000_0000_7FFF_F000;
-pub const USER_STACK_PAGES: u64 = 4;
+pub const USER_STACK_PAGES: u64 = 256; // 1mb stack
 pub const USER_STACK_INITIAL_SLACK: u64 = 0x100;
