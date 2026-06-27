a369495a1f7b26d1872a669fc4aa03a4ea9663a7
xjunko <phony@kafu.ovh>
2026-06-27 19:16:36 +0800

    kernel/layout: `PROCESS_STACK_SIZE`

--- flower-kernel/src/arch/x86_64/layout.rs
+++ flower-kernel/src/arch/x86_64/layout.rs
@@ -1,5 +1,7 @@
 pub const PAGE_SIZE: usize = 4096;
 
+pub const PROCESS_STACK_SIZE: usize = PAGE_SIZE * 4;
+
 pub const KERNEL_HEAP_START: usize = 0xFFFF_9000_0000_0000;
 pub const KERNEL_HEAP_SIZE: usize = 1024 * 1024;
 
